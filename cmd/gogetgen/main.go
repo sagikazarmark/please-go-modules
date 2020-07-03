fd80a8ea9162b110e25dea7b7ac1fda12e1fc78e
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-07-03 22:34:40 +0200

    Partial dir implementation

--- cmd/gogetgen/main.go
+++ cmd/gogetgen/main.go
@@ -4,11 +4,21 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"strings"
+)
+
+var (
+	stdout = flag.Bool("stdout", false, "Dump rules to the standard output")
+	dir    = flag.String("dir", "", "Dump rules into a directory")
+	clean  = flag.Bool("clean", false, "Clean target before generating new rules")
 )
 
 type ModuleRoot struct {
@@ -18,6 +28,8 @@ type ModuleRoot struct {
 }
 
 func main() {
+	flag.Parse()
+
 	// TODO: find a better way to determine the root module name
 	var rootModule string
 	{
@@ -120,32 +132,33 @@ func main() {
 		moduleRoot.Sum = sum
 	}
 
-	for _, modulePath := range moduleKeys {
-		module := modules[modulePath]
+	if *stdout {
+		for _, modulePath := range moduleKeys {
+			module := modules[modulePath]
 
-		var install []string
-		depList := make(map[string]bool)
+			var install []string
+			depList := make(map[string]bool)
 
-		if len(module.Packages) > 0 {
-			for _, pkg := range module.Packages {
-				install = append(install, fmt.Sprintf("%q", strings.TrimPrefix(strings.TrimPrefix(pkg.ImportPath, module.Module.Path), "/")))
+			if len(module.Packages) > 0 {
+				for _, pkg := range module.Packages {
+					install = append(install, fmt.Sprintf("%q", strings.TrimPrefix(strings.TrimPrefix(pkg.ImportPath, module.Module.Path), "/")))
 
-				for _, imp := range pkg.Imports {
-					if packages[imp].Standard {
-						continue
+					for _, imp := range pkg.Imports {
+						if packages[imp].Standard {
+							continue
+						}
+
+						depList[pkgToModule[imp]] = true
 					}
-
-					depList[pkgToModule[imp]] = true
 				}
 			}
-		}
 
-		var deps []string
-		for dep, _ := range depList {
-			deps = append(deps, fmt.Sprintf("%q", ":"+strings.Replace(dep, "/", "_", -1)))
-		}
+			var deps []string
+			for dep, _ := range depList {
+				deps = append(deps, fmt.Sprintf("%q", ":"+strings.Replace(dep, "/", "_", -1)))
+			}
 
-		fmt.Printf(`go_module_get(
+			fmt.Printf(`go_module_get(
 	name = "%s",
 	get = "%s",
 	version = "%s",
@@ -154,12 +167,83 @@ func main() {
 	deps = [%s],
 	visibility=["PUBLIC"],
 )`+"\n\n",
-			strings.Replace(module.Module.Path, "/", "_", -1),
-			module.Module.Path,
-			module.Module.Version,
-			module.Sum,
-			strings.Join(install, ", "),
-			strings.Join(deps, ", "),
-		)
+				strings.Replace(module.Module.Path, "/", "_", -1),
+				module.Module.Path,
+				module.Module.Version,
+				module.Sum,
+				strings.Join(install, ", "),
+				strings.Join(deps, ", "),
+			)
+		}
+	} else if *dir != "" {
+		if filepath.IsAbs(*dir) {
+			log.Fatal("Absolute path not allowed")
+		}
+
+		// TODO: disable every path outside of the module root
+
+		if *clean {
+			err := os.RemoveAll(*dir)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		err := os.MkdirAll(*dir, 0755)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, modulePath := range moduleKeys {
+			module := modules[modulePath]
+
+			var install []string
+			depList := make(map[string]bool)
+
+			if len(module.Packages) > 0 {
+				for _, pkg := range module.Packages {
+					install = append(install, fmt.Sprintf("%q", strings.TrimPrefix(strings.TrimPrefix(pkg.ImportPath, module.Module.Path), "/")))
+
+					for _, imp := range pkg.Imports {
+						if packages[imp].Standard {
+							continue
+						}
+
+						depList[pkgToModule[imp]] = true
+					}
+				}
+			}
+
+			var deps []string
+			for dep, _ := range depList {
+				if path.Dir(modulePath) == path.Dir(dep) {
+					deps = append(deps, fmt.Sprintf("%q", ":"+path.Base(dep)))
+				} else {
+					deps = append(deps, fmt.Sprintf("%q", fmt.Sprintf("//%s:%s", path.Join(*dir, path.Dir(dep)), path.Base(dep))))
+				}
+			}
+
+			fmt.Printf(`%s
+
+go_module_get(
+	name = "%s",
+	get = "%s",
+	version = "%s",
+	sum = "%s",
+	install = [%s],
+	deps = [%s],
+	visibility=["PUBLIC"],
+)`+"\n\n",
+				path.Join(*dir, path.Dir(modulePath), "BUILD"),
+				path.Base(modulePath),
+				module.Module.Path,
+				module.Module.Version,
+				module.Sum,
+				strings.Join(install, ", "),
+				strings.Join(deps, ", "),
+			)
+		}
+	} else {
+		log.Fatal("Either -stdout or -dir must be passed")
 	}
 }
