7ec4d6edd9e8fb97481201300c2f76864846cfd1
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-08-20 20:36:52 +0200

    Improve go mod download
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ example2/liba/liba.go
@@ -0,0 +1,5 @@
+package liba
+
+import "github.com/sagikazarmark/please-go-modules/example2/libb"
+
+const HelloWorld = "Hello " + libb.World + "!"
---
+++ example2/libb/libb.go
@@ -0,0 +1,3 @@
+package libb
+
+const World = "world"
