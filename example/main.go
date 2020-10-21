b3cf922f3d4f9140c967bd253368baef4ba85ddb
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-10-21 16:07:04 +0200

    test: add failing scenario
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- example/main.go
+++ example/main.go
@@ -4,6 +4,7 @@ import (
 	_ "emperror.dev/errors/match"
 	_ "github.com/golang/snappy"
 	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/opencontainers/runc/libcontainer/system"
 	_ "golang.org/x/sys/unix"
 	_ "google.golang.org/grpc"
 )
